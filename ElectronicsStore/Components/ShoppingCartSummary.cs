294f88455b3b832e75ba566358f690d73c8b0b15
Panagiotis Nomikos <102253139+PeterNom@users.noreply.github.com>
2024-03-05 11:45:29 +0200

    Added the shopping cart ViewComponent controller.

---
+++ ElectronicsStore/Components/ShoppingCartSummary.cs
@@ -0,0 +1,26 @@
+﻿using ElectronicsStore.Models.ViewModels;
+using ElectronicsStore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicsStore.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var items = await _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
